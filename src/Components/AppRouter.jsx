fa6b73e4924c58537020e11f4b124c9c24042678
Jessica Rodriguez <jessica.g.rodriguez.chim@gmail.com>
2023-08-15 22:37:39 -0600

    add about

--- src/Components/AppRouter.jsx
+++ src/Components/AppRouter.jsx
@@ -3,11 +3,13 @@ import { Login } from './Login';
 import { Register } from './Register'
 import { HomePage } from './HomePage'
 import { PrivateRoute } from './PrivateRoute';
+import {About} from './About';
 
 export const AppRouter = () =>{
     return <>
         <Routes>
             <Route path='/' element={<Login/>}/>
+            <Route path='AboutUs' element={<About/>}/>
             <Route path='Register' element={<Register/>}/>
             <Route 
                 path='HomePage' 

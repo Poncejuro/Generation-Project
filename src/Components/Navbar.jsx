899db0a2b8d93bbee684404d3116e3d94f2b1c23
Jessica Rodriguez <jessica.g.rodriguez.chim@gmail.com>
2023-08-18 21:21:38 -0600

    fixclassName

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,11 +24,11 @@ export const NavBar = () => {
                 <li className="nav-item">
 
                   <div className='search-group'>
-                    <div class="box-search">
-                      <input type="search" id="form1" class="form-control" placeholder='Search'/>
+                    <div className="box-search">
+                      <input type="search" id="form1" className="form-control" placeholder='Search'/>
                     </div>
-                    <button type="button" class="btn btn-primary">
-                      <i class="fas fa-search"></i>
+                    <button type="button" className="btn btn-primary">
+                      <i className="fas fa-search"></i>
                     </button>
                   </div>
                   

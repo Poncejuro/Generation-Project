6138c6e9a8665b379d6fcecac36d0d8672920527
Luis Ponce <luis_26210@hotmail.com>
2023-08-22 13:24:27 -0600

    team start

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,4 +1,5 @@
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import '../styles/Contact.css';
 
 
@@ -8,28 +9,32 @@ export const Contact = () => {
         <>
             <div className='container'>
                 <div className='div-principal row align-items-center justify-content-center ' style={{ height: "100vh" }}>
-                    <div className='col-6'>
-                        <form className='needs-validation'>
-                            <div className="mb-3 col-md-12">
-                                <label htmlFor ="validationCustom01" className="form-label">Name</label>
-                                <input placeholder='Juanito Pérez' type="text" className="form-control" id="validationCustom01" aria-describedby="emailHelp" />
-                            </div>
-                            <div className="mb-3 col-md-12">
-                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input placeholder='example@gmail.com' type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" />
-                            </div>
-                            <div className="mb-3 col-md-12">
-                                <label htmlFor="exampleInputMessage" className="form-label">Message</label>
-                                <input placeholder='Escribe aquí tu mensaje' type="text" className="form-control py-4" id="inputMessage" />
-                            </div>
-                            <div className="mb-3 col-md-12">
-                                <label htmlFor="exampleInputTel" className="form-label">Telephone </label>
-                                <input placeholder='5512345678' type="tel" className="form-control" id="inputTel" aria-describedby="emailHelp" />
-                            </div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control type="text" placeholder="Juanito Pérez" />
+                    </Form.Group>
 
-                            <button className="btn btn-primary" type="submit">Submit</button>
-                        </form>
-                    </div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Correo Electrónico</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Mensaje</Form.Label>
+                        <Form.Control type="text" placeholder="mensaje" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Teléfono</Form.Label>
+                        <Form.Control type="text" placeholder="telefono" />
+                    </Form.Group>
+
+                    
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
                 </div>
             </div>
 

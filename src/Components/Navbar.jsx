34c42f433c47e6895b731312c7e7e0b8caaa06b8
Esteban Castro <eacq.andres@gmail.com>
2023-08-25 21:54:02 -0500

    navbar grid

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import '../styles/Navbar.css';
 
 export const NavBar = () => {
 
@@ -13,30 +14,36 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Form className="d-flex">
-            <Form.Control
-              type="search"
+            <Form.Control 
+              type="search..."
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+
             />
             <Button variant="outline-success">Search</Button>
           </Form>
 
-          <Nav
-            className="me-auto my-2 my-lg-0 text-right d-flex w-100"
-            style={{ maxHeight: "100px", fontSize: "24px"}}
-            navbarScroll
+          <div className='elementsNav'>
+            <Nav
+              className="me-auto my-2 my-lg-0 text-right d-flex w-100"
+              style={{ maxHeight: "100px", fontSize: "24px" }}
+              navbarScroll
 
-          >
-            <Nav.Link href="#action1">Eventos</Nav.Link>
-            <Nav.Link href="#action2">Personas</Nav.Link>
-            <Nav.Link href="#action1">Contacto</Nav.Link>
-            <Nav.Link href="#action2">Perfil</Nav.Link>
-            <Nav.Link href="#action1">Cuenta</Nav.Link>
+            >
+              <Nav.Link href="#action1">Eventos</Nav.Link>
+              <Nav.Link href="#action2">Personas</Nav.Link>
+              <Nav.Link href="#action1">Contacto</Nav.Link>
+              <Nav.Link href="#action2">Perfil</Nav.Link>
+              <Nav.Link href="#action1">Cuenta</Nav.Link>
 
 
 
-          </Nav>
+            </Nav>
+
+          </div>
+
+
 
         </Navbar.Collapse>
       </Container>

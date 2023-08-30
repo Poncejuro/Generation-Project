7589c19275a77f42c8aacab357b5bbe61b30fc47
Leonardo Velazquez <leovelazquez812@gmail.com>
2023-08-30 17:49:49 -0600

    ci: add more functionalities to Profile

--- src/Components/FeedCards/FeedCards.jsx
+++ src/Components/FeedCards/FeedCards.jsx
@@ -1,6 +1,55 @@
+// import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Box from "@mui/material/Box";
+// import Grid from "@mui/material/Unstable_Grid2";
+
+// export const FeedCards = ({
+//   title,
+//   text,
+//   cardSizeW,
+//   cardSizeH,
+//   logo,
+//   alt,
+//   imgSize,
+// }) => {
+//   return (
+//     <>
+//       {/* <div className="card" style={{ width: cardSizeW, height: "auto" }}> */}
+//       <div className="card">
+//         {/* <Card> */}
+//         <Box
+//           sx={{
+//             m: 2,
+//             // display: "flex",
+//             // justifyContent: "center",
+//             // alignItems: "center",
+//             // alignContent: "center",
+//           }}
+//         >
+//           <Grid container spacing={3}>
+//             <Grid xs={3}>
+//               {/* <CardMedia sx={{ height: 140 }} image={logo} /> */}
+//               <img src={logo} style={{ width: cardSizeH, height: imgSize }} />
+//             </Grid>
+//             <Grid xs={9}>
+//               {/* <CardContent> */}
+//               <h3>{title}</h3>
+//               <p>{text}</p>
+//               {/* </CardContent> */}
+//             </Grid>
+//           </Grid>
+//         </Box>
+//         {/* </Card> */}
+//       </div>
+//     </>
+//   );
+// };
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
 
 export const FeedCards = ({
   title,
@@ -10,28 +59,25 @@ export const FeedCards = ({
   logo,
   alt,
   imgSize,
+  rol,
 }) => {
   return (
     <>
       <div className="card" style={{ width: cardSizeW, height: "auto" }}>
-        {/* <Row> */}
-          {/* <Col xs={12} md={3} className="col-imgr m-1">
+        <Stack direction="horizontal" gap={3}>
+          <div className="p-2 col-xs-12 col-md-3">
             <img src={logo} style={{ width: imgSize }} alt={alt} />
-          </Col>
-          <Col xs={12} md={9} className="m-1">
+          </div>
+          <div className="p-2 col-xs-12 col-md-9">
             <h3>{title}</h3>
+            <p>{rol}</p>
             <p>{text}</p>
-          </Col> */}
-          <Stack direction="horizontal" gap={3}>
-            <div className="p-2 col-xs-12 col-md-3">
-              <img src={logo} style={{ width: imgSize }} alt={alt} />
-            </div>
-            <div className="p-2 col-xs-12 col-md-9">
-              <h3>{title}</h3>
-              <p>{text}</p>
-            </div>
-          </Stack>
-        {/* </Row> */}
+          </div>
+        </Stack>
+
+        <Button variant="success" href="#">
+          Eliminar
+        </Button>
       </div>
     </>
   );
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,19 +1,50 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Form from "react-bootstrap/Form";
-import Stack from "react-bootstrap/Stack";
+// import Stack from "react-bootstrap/Stack";
 import React, { useState } from "react";
 import { NavBar } from "../../Components/Navbar";
 import { FeedCards } from "../../Components/FeedCards/FeedCards";
 import img1 from "../../Img/2sd.jpg";
+//from MUI React
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Unstable_Grid2";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import { palette } from "@mui/system";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 import "./Profile.css";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicStack() {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stack spacing={2}>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </Box>
+  );
+}
 
 export const Profile = () => {
   const [form, setForm] = useState({});
@@ -41,17 +72,24 @@ export const Profile = () => {
     setMiArray([...miArray, objectData]);
   };
 
-
-
-  const [userText, setUserText] = useState('');
+  const [userText, setUserText] = useState("");
 
   const handleTextChange = (event) => {
     setUserText(event.target.value);
-  }
+  };
 
   const editEvent = (event) => {
     setUserText(event.target.value);
-  }
+  };
+
+  let [profileButton, setProfileButton] = useState(false);
+  // let [saveButton, setSaveButton] = useState(false);
+  //getting the value of the button profile and activate the textareas
+  //using useState
+  const handleProfileButton = (event) => {
+    // setProfileButton(true);
+    setProfileButton(!profileButton);
+  };
 
   return (
     <>
@@ -60,160 +98,257 @@ export const Profile = () => {
       <div className="body-Profile">
         <Row>
           <Col xs={12} sm={12} md={3} className="flow-box">
-            
-            <div className="img-box-profile">
-              <ListGroup.Item className="text-center border-0">
+            <div className="img-box-profile p-2 text-center">
+              <ListGroup.Item>
                 <Image
                   src="https://bootdey.com/img/Content/avatar/avatar1.png"
                   fluid
                   roundedCircle
-                  />
+                />
               </ListGroup.Item>
             </div>
 
-            <div className="bio-box-profile-edit" style={{display:'none'}}>
-              <ListGroup.Item className=" Principal-Container text-center text-light border-0">
-                <textarea
-                  value={userText}
-                  onChange={handleTextChange}
-                  placeholder="Escribe tu nombre"
-                  style={{marginTop:'1vh', marginBottom:'1vh'}}
-                />
-                <textarea
-                  value={userText}
-                  onChange={handleTextChange}
-                  placeholder="escribe tu rol"
-                />
-                <textarea
-                  value={userText}
-                  onChange={handleTextChange}
-                  placeholder="Agrega una pequeña biografía"
-                  style={{marginTop:'1vh', marginBottom:'1vh'}}
-                />
-                </ListGroup.Item>
-                <Form.Group controlId="formFile" className="mb-3 ">
-                      <Form.Control type="file"/>
-                </Form.Group>
+            <div
+              className="bio-box-profile-edit"
+              style={{ display: profileButton ? "inline" : "none" }}
+            >
+              <ListGroup.Item
+                className="Principal-Container"
+                // sx={{ p: 1.5, m: 1.5 }}
+              >
+                {/* <ListGroup.Item className="text-center text-light border-0"> */}
+                <Stack>
+                  <TextField
+                    id="nombreUsuario"
+                    label="Escribe tu nombre"
+                    variant="filled"
+                    defaultValue=""
+                    // value={userText}
+                    sx={{
+                      p: 1.5,
+                      m: 1.5,
+                      input: { color: "black" },
+                      background: "#FFF",
+                      label: { color: "black" },
+                      // color: "text.primary",
+                    }}
+                  />
+                  <TextField
+                    id="nombreUsuario"
+                    label="Escribe tu rol"
+                    variant="filled"
+                    defaultValue=""
+                    // value={userText}
+                    sx={{
+                      p: 1.5,
+                      m: 1.5,
+                      input: { color: "black" },
+                      background: "#FFF",
+                      label: { color: "black" },
+                      // color: "text.primary",
+                    }}
+                  />
+                  <TextField
+                    id="nombreUsuario"
+                    label="Autobiografía"
+                    variant="filled"
+                    defaultValue=""
+                    multiline
+                    rows={2}
+                    // value={userText}
+                    sx={{
+                      p: 1.5,
+                      m: 1.5,
+                      input: { color: "black" },
+                      background: "#FFF",
+                      label: { color: "black" },
+                      // color: "text.primary",
+                    }}
+                  />
+                  {/* <input
+                    className="p-2 m-3"
+                    value={userText}
+                    onChange={handleTextChange}
+                    placeholder="Escribe tu nombre"
+                  />
+                  <input
+                    className="p-2 m-3"
+                    value={userText}
+                    onChange={handleTextChange}
+                    placeholder="escribe tu rol"
+                  />
+                  <textarea
+                    className="p-2 m-3"
+                    value={userText}
+                    onChange={handleTextChange}
+                    placeholder="Agrega una pequeña biografía"
+                    rows={2}
+                  /> */}
+                  <Form.Group controlId="formFile" className="mb-3 p-2 m-3">
+                    <Form.Control type="file" />
+                  </Form.Group>
+                </Stack>
+              </ListGroup.Item>
             </div>
-            
-            <div className="bio-box-profile-save">
-              <ListGroup.Item className=" Principal-Container text-center text-light border-0">
-                <h4 className=" text-profile text-capitalize text-light">Juan González</h4>
-                <h5 className=" text-profile text-capitalize text-light">Bajista</h5>
-                <Card.Text className=" text-profile-2">
-                        This is a longer card with supporting text below as a
-                        natural lead-in to additional content. This content is a
-                        little bit longer. asdasdasdadas
-                        This is a longer card with supporting text below as a
-                        natural lead-in to additional content. This content is a
-                        little bit longer. asdasdasdadas
-                      </Card.Text>
-                </ListGroup.Item>
-            </div>
-            
 
-            <ListGroup.Item className="text-center border-0">
-              <Button variant="success" href="#">
+            <div
+              className="bio-box-profile-save"
+              style={{ display: !profileButton ? "inline" : "none" }}
+            >
+              <ListGroup.Item className=" Principal-Container text-center text-light border-0">
+                <h4 className=" text-profile text-capitalize text-light">
+                  Juan González
+                </h4>
+                <h5 className=" text-profile text-capitalize text-light">
+                  Bajista
+                </h5>
+                <p className=" text-profile-2">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer. asdasdasdadas This is a longer card with supporting
+                  text below as a natural lead-in to additional content. This
+                  content is a little bit longer. asdasdasdadas
+                </p>
+              </ListGroup.Item>
+            </div>
+
+            <ListGroup.Item className="text-center ">
+              <Button
+                className="m-2"
+                variant="success"
+                onClick={handleProfileButton}
+                style={{ display: profileButton ? "none" : "inline" }}
+              >
                 Editar perfil
               </Button>
-              <Button variant="success" href="#" style={{display:'none'}}>
+              <Button
+                className="m-2"
+                variant="success"
+                onClick={handleProfileButton} // the function of hide and show it's done in this button
+                style={{ display: profileButton ? "inline" : "none" }}
+              >
                 Guardar cambios
               </Button>
             </ListGroup.Item>
           </Col>
 
-          <Col xs={12} sm={12} md={9} className="flow-box">
-            
-            <section className="buttons-container">
-
-            <Card className="p-1" border="0">
-                <Card.Body className="p-1" border="0">
-                  <ButtonGroup aria-label="Basic example">
-                    <Button variant="success" href="#">
-                      Inicio
-                    </Button>
+          <Col xs={12} sm={12} md={9}>
+            {/* <section className="buttons-container text-center"> */}
+            <Box
+              sx={{
+                m: 3,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid xs={12}>
+                  <Stack direction="row" spacing={4}>
                     <Button variant="success" href="#">
                       Amigos
                     </Button>
                     <Button variant="success" href="#">
-                      Estado
-                    </Button>
-                    <Button variant="success" href="#">
                       Mis eventos
                     </Button>
-                  </ButtonGroup>
-                </Card.Body>
-            </Card>
-            </section>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </Box>
+            {/* </section> */}
 
-         
-            <form >
-              <section className="form-box">
-                <Form.Control
-                      as="textarea"
-                      placeholder="Título de la publicación"
-                      name="text"
-                      onChange={setField}
-                      style={{ width: '650px'}}
-                      />
-                <Form.Control
-                        as="textarea"
-                        placeholder="Pie de imagen"
-                        name="text"
-                        onChange={setField}
-                        style={{ width: '650px'}}
-                        />       
-              </section> 
-
-              <section className="form-box-2">
-              <Form.Control
+            <form>
+              <Grid container spacing={2} sx={{ m: 1, p: 1 }}>
+                <Grid xs={12} md={6}>
+                  <Form.Control
+                    as="input"
+                    placeholder="Título de la publicación"
+                    name="text"
+                    onChange={setField}
+                  />
+                </Grid>
+                <Grid xs={12} md={6}>
+                  <Form.Control
+                    as="input"
+                    placeholder="Pie de imagen"
+                    name="text"
+                    onChange={setField}
+                  />
+                </Grid>
+                <Grid xs={12} md={12}>
+                  <Form.Control
                     as="textarea"
                     placeholder="Detalles del evento"
                     name="text"
+                    rows={4}
                     onChange={setField}
-                    style={{ width: '1300px', height: '30vh'}}
-              /> 
-              </section>
-
-              <section className="form-box-2">
-                <Stack direction="horizontal" style={{ width: '1300px'}}>
-                    <Form.Group controlId="formFile" className="mb-3 ">
-                      <Form.Control type="file"/>
-                    </Form.Group>
-                    <Button className="ms-auto" variant="success" type="file">
+                  />
+                </Grid>
+              </Grid>
+              <Grid xs={12}>
+                <Row>
+                  <Box
+                    sx={{
+                      m: 1,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      alignContent: "center",
+                    }}
+                  >
+                    <Grid container spacing={3}>
+                      {/* <Stack direction="row" spacing={1}> */}
+                      <Form.Group controlId="formFile" className="m-1">
+                        <Form.Control type="file" />
+                      </Form.Group>
+                      {/* <Button className="ms-auto" variant="success" type="file">
                       Foto/Video
-                    </Button>
-                    <Button variant="success" href="#" onClick={saveFeed}>
-                      Publicar
-                    </Button>
-                  </Stack>
-              </section>
+                    </Button> */}
+                      <Button
+                        // className="ms-auto"
+                        variant="success"
+                        href="#"
+                        onClick={saveFeed}
+                      >
+                        Publicar
+                      </Button>
+                      {/* </Stack> */}
+                    </Grid>
+                  </Box>
+                </Row>
+              </Grid>
             </form>
 
             <section className="form-box-2">
-              {/* <Form.Select size="lg"> */}
-              {/* <option>Mas reciertes</option>
-                <option>Mas antiguos</option>
-                <option>Etc</option> */}
-              <Row className="p-1 mt-0 ">
-                <ButtonGroup aria-label="Basic example" style={{ width: '1320px'}}>
-                  <Button
-                    className="me-1"
-                    variant="success"
-                    href="#"
-                    onClick={updateData}
-                  >
-                    Actualizar
-                  </Button>
-                  <Button className="me-1" variant="success" href="#">
-                    Reciente
-                  </Button>
-                  <Button className="me-6" variant="success" href="#">
-                    Antiguo
-                  </Button>
-                </ButtonGroup>
+              <Row>
+                <Box
+                  sx={{
+                    m: 2,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignContent: "center",
+                  }}
+                >
+                  <Grid container spacing={2}>
+                    <Button
+                      className="me-1"
+                      variant="success"
+                      href="#"
+                      onClick={updateData}
+                    >
+                      Actualizar
+                    </Button>
+                    <Button className="me-1" variant="success" href="#">
+                      Reciente
+                    </Button>
+                    <Button className="me-6" variant="success" href="#">
+                      Antiguo
+                    </Button>
+                  </Grid>
+                </Box>
               </Row>
-              {/* </Form.Select> */}
 
               <div className="feed-box">
                 {miArray.map((aux) => (
@@ -226,9 +361,7 @@ export const Profile = () => {
                   />
                 ))}
               </div>
-              
             </section>
-
           </Col>
         </Row>
       </div>

ed4a0ffe889eedc2ed19fdb998031108a3d3d993
Jessica Rodriguez <jessica.g.rodriguez.chim@gmail.com>
2023-09-06 17:35:05 -0600

    tarea iniciada

--- src/Pages/Login/Register.jsx
+++ src/Pages/Login/Register.jsx
@@ -1,20 +1,24 @@
 import "./login.css";
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 export const Register = () => {
-  const [datos, setDatos] = useState({
-    password: "",
-    correo: "",
-  });
-
+  const [datos, setDatos] = useState({});
+  const [textError, setTextError] = useState("");
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     setDatos({
       ...datos,
-      [event.target.name]: event.target.value,
+      [event.target.name]: event.target.value.trim(),
     });
+    if (datos.user_password !== datos.user_confirmPassword) {
+      console.log("Las contraseñas no conciden");
+      setTextError("Las contraseñas no conciden");
+    } else {
+      setTextError("");
+      console.log("Si sirve");
+    }
   };
 
   const sendData = (event) => {
@@ -25,6 +29,7 @@ export const Register = () => {
         logged: true,
       },
     });
+    localStorage.setItem("userData", JSON.stringify(datos));
   };
 
   return (
@@ -57,11 +62,22 @@ export const Register = () => {
                     />
                   </div>
                   <div className="mb-3">
-                    <label className="form-label">Crea tu cuenta</label>
+                    <label className="form-label">Numero telefonico</label>
+                    <input
+                      type="tel"
+                      className="form-control control-aux"
+                      name="user_tel"
+                      pattern="[0-9]{10}"
+                      required
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Ingresa una nueva contraseña</label>
                     <input
                       type="password"
                       className="form-control control-aux"
-                      name="user_tel"
+                      name="user_password"
                       required
                       onChange={handleInputChange}
                     />
@@ -69,11 +85,13 @@ export const Register = () => {
                     <input
                       type="password"
                       className="form-control control-aux"
-                      name="user_tel"
+                      name="user_confirmPassword"
                       required
                       onChange={handleInputChange}
                     />
+                    <h3>{textError}</h3>
                   </div>
+                 
                   <button id="btn" type="submit" className="btn btn-primary">
                     Inicia sesión
                   </button>

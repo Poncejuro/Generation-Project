8900923f68dea56f4196e27541cf020a42adf071
Norma Enriquez <normalety455@gmail.com>
2023-09-26 17:47:51 -0600

    Responsividad de botones

--- src/Pages/Events/EventsCards/EventsCards.jsx
+++ src/Pages/Events/EventsCards/EventsCards.jsx
@@ -10,19 +10,18 @@ export const EventsCards = ({
         <img src={img} alt={alt}  /*style={{ width: '150px', height: '150px', margin: 'auto', display:'block' }}*/
         />
         <h2>{title}</h2>
-        <h2>{footer}</h2>
-        <h2>{event_details}</h2>
-        <Button id="eventCardsButtons" variant="success" href="#">
+        <h3>{footer}</h3>
+        <h3 className="Event_details">{event_details}</h3>
+        
+
+         <Button id="eventCardsButtons" className="me-1" variant="success" href="#">
           Me interesa
         </Button>
-        <Button id="eventCardsButtons1" variant="success" href="#">
+        <Button id="eventCardsButtons1" className="me-1" variant="success" href="#">
           No me interesa
         </Button>
-
-
-
-
-      </div>
+        </div>
+        
     </>
   );
 };

f942f12fbb03a11102867c75e328e4d7c9189917
Esteban Castro <eacq.andres@gmail.com>
2023-09-27 19:36:58 -0500

    get profile events

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,25 +1,50 @@
 import { NavBar } from "../Navbar"
 import { EventsCards } from "./EventsCards/EventsCards";
-import React from "react";
-import eventsData from "../../utils/events";
 
+import eventsData from "../../utils/events";
+import axios from "axios";
+import React, { useEffect,useState } from "react";
 
 
 
 export const Events = () => {
-  const events = eventsData;
+
+  ///////////////////////////////API EXAMPLE//////////////////////
+  const [dataBack, setDataBack] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Realiza una solicitud GET a la API
+    axios.get('http://localhost:5000/api/v1/publications')
+      .then(response => {
+        setDataBack(response.data);
+        setLoading(false);
+        console.log('response data');
+        console.log(response.data);
+        console.log('hook back');
+        console.log(dataBack);
+      })
+      .catch(error => {
+        console.error('Error al obtener datos:', error);
+        setLoading(false);
+      });
+  }, []);
+  ///////////////////////////////////////////////////////////////
+
+  const events = dataBack;
 
   const updatedFriendList = [...events];
 
   for (let i = 0; i < updatedFriendList.length; i++) {
     updatedFriendList[i].img = `https://picsum.photos/200?random=${i}`;
   }
+  
 
 
   return (
     <>
       <NavBar />
-      <div  id= "events-container " className="body-events align-content-center">
+      <div id="events-container " className="body-events align-content-center">
         <div className="EventsCard">
           {events.map((event, index) => (
             <EventsCards
@@ -27,7 +52,7 @@ export const Events = () => {
               img={event.img}
               title={event.title}
               footer={event.footer}
-              event_details={event.event_details}
+              event_details={event.eventDetails}
 
             />
           ))}
--- src/Pages/Friends/Friends.jsx
+++ src/Pages/Friends/Friends.jsx
@@ -2,11 +2,35 @@ import { FriendCards } from "./FriendCards/FriendCards";
 import { NavBar } from "../Navbar";
 import { friendList } from "../../utils/friends";
 import './FriendCards/FriendCards.css'
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 
 export const Friends = () => {
 
+  ///////////////////////////////API EXAMPLE//////////////////////
+  const [dataBack, setDataBack] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Realiza una solicitud GET a la API
+    axios.get('http://localhost:5000/api/v1/profiles')
+      .then(response => {
+        setDataBack(response.data);
+        setLoading(false);
+        console.log('response data');
+        console.log(response.data);
+        console.log('hook back');
+        console.log(dataBack);
+      })
+      .catch(error => {
+        console.error('Error al obtener datos:', error);
+        setLoading(false);
+      });
+  }, []);
+  ///////////////////////////////////////////////////////////////
+
+  /*
   function App() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -25,25 +49,28 @@ export const Friends = () => {
         });
     }, []);
   }
+  */
+
+  const profiles = dataBack;
 
   //Se agrego una api para generar imagenes aleatorias
-  const updatedFriendList = [...friendList];
+  const updatedFriendList = [...profiles];
 
   for (let i = 0; i < updatedFriendList.length; i++) {
-    updatedFriendList[i].photo = `https://picsum.photos/200?random=${i}`;
+    updatedFriendList[i].photo2 = `https://picsum.photos/200?random=${i}`;
   }
 
   return (
     <>
       <NavBar />
       <section className="friends-container">
-        <div className="friendsCard">{friendList.map((friend, index) => (
+        <div className="friendsCard">{profiles.map((friend, index) => (
           <FriendCards
             key={index}
             name={friend.name}
-            rol={friend.rol}
-            bio={friend.bio}
-            photo={friend.photo}
+            rol={friend.role}
+            bio={friend.biography}
+            photo={friend.photo2}
             alt={"aqui debe haber una foto"}
           />
         ))}</div>
